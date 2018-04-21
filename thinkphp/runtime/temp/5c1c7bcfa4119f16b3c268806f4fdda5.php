a3ca238b234711f9795c3c979c40608f5e1b5f46
sigmeta <sunhao19940302@qq.com>
2018-04-21 21:05:59 +0800

    SecondVersion
    Second Version

---
+++ thinkphp/runtime/temp/5c1c7bcfa4119f16b3c268806f4fdda5.php
@@ -0,0 +1,99 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:104:"E:\sigmeta\Documents\GitHub\FirstVersion\thinkphp\public/../application/index\view\index\infomation.html";i:1523248055;s:90:"E:\sigmeta\Documents\GitHub\FirstVersion\thinkphp\application\index\view\index\header.html";i:1523199642;s:90:"E:\sigmeta\Documents\GitHub\FirstVersion\thinkphp\application\index\view\index\footer.html";i:1523201959;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="/thinkphp/public/static/css/basecss.css" />
+    <script type="text/javascript">
+
+        function validate_required(field,alerttxt)
+        {
+            with (field)
+            {
+                if (value==null||value=="")
+                {alert(alerttxt);return false}
+                else {return true}
+            }
+        }
+
+        function validate_form(thisform)
+        {
+            with (thisform)
+            {
+                if (validate_required(id,"学号不能为空！")==false)
+                {id.focus();return false}
+            }
+        }
+    </script>
+</head>
+
+<body>
+
+<div class="header">
+    <img src="/thinkphp/public/static/image/logo-v1.png">
+    <!-- <h1>北京大学软件与微电子学院校友会</h1> -->
+</div>
+<div class="row">
+
+    <div class="col-2 menu">
+        <ul>
+            <li><a href="/thinkphp/public/index.php?s=index/index/home">首页</a></li>
+            <li>学院新闻</li>
+            <li>校友活动</li>
+            <li>校友新闻</li>
+        </ul>
+    </div>
+    <div class="col-1">
+    </div>
+
+<div class="col-6 ">
+    <h2>个人信息</h2>
+    <form action="/thinkphp/public/index.php?s=/index/index/changeinfo" method="post">
+        姓名: <?php echo $list['姓名']; ?><br>
+        学号: <?php echo $list['学号']; ?><br>
+        方向: <?php echo $list['方向']; ?><br>
+        导师: <?php echo $list['导师']; ?><br>
+        入学: <?php echo $list['年级（入学）']; ?>年<br>
+        籍贯: <?php echo $list['籍贯']; ?><br>
+        手机: <?php echo $list['手机']; ?><br>
+        邮箱: <?php echo $list['邮箱']; ?><br>
+        省份: <?php echo $list['省份']; ?><br>
+        城市: <?php echo $list['城市']; ?><br>
+        地址: <?php echo $list['通讯地址']; ?><br>
+        行业: <?php echo $list['行业']; ?><br>
+        单位: <?php echo $list['现工作单位']; ?><br>
+        职务: <?php echo $list['职务']; ?><br>
+
+        <input type="submit"  value="完善个人信息">
+    </form>
+</div>
+
+<div class="col-1">
+</div>
+
+<div class="col-2 menu">
+    <form action="/thinkphp/public/index.php?s=/index/index/logout" method="post">
+        <input type="button" value="您好，<?php echo $username; ?>" onclick="">
+        <input value="退出" type="submit">
+    </form>
+    <ul>
+    <li><a href="/thinkphp/public/index.php?s=/index/index/information">个人中心</a></li>
+    <li>账户信息</li>
+    <li>我的活动</li>
+    <li>我的名片</li>
+</ul>
+</div>
+
+</div>
+
+<div class="bottom">
+    <h3>友情链接
+        <a href="http://www.ss.pku.edu.cn/">北京大学软件与微电子学院</a>
+        <a href="http://www.pku.edu.cn/">北京大学</a>
+        <a href="http://www.pku.org.cn/">北京大学校友网</a>
+        <a href="http://www.ss.pku.edu.cn/">北京大学软件与微电子学院</a>
+    </h3>
+</div>
+</html>
